c93dc3b5e30c778682b47af6aa2bbe620fd395aa
Aric Rachmany <Aric.Rachmany@dh.com>
2022-03-18 17:52:31 +0200

    node

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<points-counter>'));
+app.use(express.static(__dirname + '/dist/points-counter'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/<points-counter>/index.html'));});
-app.listen(process.env.PORT || 4200);
+    '/dist/points-counter/index.html'));});
+app.listen(process.env.PORT || 8080);
